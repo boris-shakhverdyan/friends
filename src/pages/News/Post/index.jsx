fce363d052141f6e640334b70d5eb8d1a31ea538
Boris Shakhverdyan <68199855+boris-shakhverdyan@users.noreply.github.com>
2023-09-10 18:17:23 +0400

    add posts in news page

---
+++ src/pages/News/Post/index.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import "./style.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faMessage, faShare } from "@fortawesome/free-solid-svg-icons";
+
+const Post = ({ id, title, body, userId, tags, reactions }) => {
+    const [author, setAuthor] = useState(null);
+    const [comments, setComments] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/users/${userId}`)
+            .then((res) => res.json())
+            .then((data) => setAuthor(data));
+
+        fetch(`https://dummyjson.com/comments/post/${id}`)
+            .then((res) => res.json())
+            .then((data) => setComments(data.comments));
+    }, [id, userId]);
+
+    if (!author || !comments) {
+        return <div className="preloader"></div>;
+    }
+
+    return (
+        <div className="post">
+            <div className="header">
+                <div className="author">
+                    <img src={author.image} alt={author.image} />
+                    <h4>{`${author.firstName} ${author.lastName} (${author.username})`}</h4>
+                </div>
+                <div className="tags">
+                    {tags.map((tag, index) => (
+                        <span key={index} className="tag">
+                            {tag}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className="content">
+                <h3>{title}</h3>
+                <p>{body}</p>
+            </div>
+            <div className="actions">
+                <button>
+                    <FontAwesomeIcon icon={faHeart} />
+                    <span>{reactions}</span>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faMessage} />
+                    <span>{0}</span>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faShare} />
+                    <span>{0}</span>
+                </button>
+            </div>
+            <div className="comments">
+                {comments
+                    ? comments.map((comment) => (
+                          <div key={comment.id} className="comment">
+                              <p>{comment.body}</p>
+                              <p className="link">{comment.user.username}</p>
+                          </div>
+                      ))
+                    : null}
+            </div>
+        </div>
+    );
+};
+
+export default Post;
--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import "./style.scss";
+import Post from "./Post";
 
 const News = () => {
-    return <div className="news">News</div>;
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/posts?limit=10")
+            .then((res) => res.json())
+            .then((data) => setPosts(data.posts));
+    }, []);
+
+    return (
+        <div className="news">
+            {posts.map((post) => (
+                <Post key={post.id} {...post} />
+            ))}
+        </div>
+    );
 };
 
 export default News;
